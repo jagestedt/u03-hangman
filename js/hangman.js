66f5197cde9bd6f677eb3030b541967b36485702
Samuel Jägestedt <ichbinjaeger@gmail.com>
2020-11-13 17:59:35 +0100

    init redo

---
+++ js/hangman.js
@@ -0,0 +1,34 @@
+// GLOBAL VARIABLES
+// add array of words and create alphabet array
+
+const wordArr = [
+  'Ashtray',
+  'Anomaly',
+  'Almonds',
+  'Ballroom',
+  'Bastard',
+  'Braille',
+  'Citizen',
+  'Chastise',
+  'Colonialism',
+  'Dickbutt',
+  'Doorbell',
+  'Diplomat',
+  'Empire',
+  'Electricity',
+  'Eel',
+  'Frankly',
+  'Focus',
+  'Figment',
+  'Gorilla',
+  'Gentleman',
+  'Gentile',
+];
+
+var count = 0;
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+// initiate empty arrays
+var blanks = [];
+var correctLetters = [];
+var incorrectLetters = [];
